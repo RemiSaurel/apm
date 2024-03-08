e915437840f0c70321711ff6772de4bfb8c5d859
RemiSaurel <r.saurel5@gmail.com>
2024-03-08 11:01:06 +0100

    style: buttons component

--- src/components/utils/ClassicButton.vue
+++ src/components/utils/ClassicButton.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
+
 const props = defineProps<{
-  title: string
+  title?: string
 }>();
 </script>
 
 <template>
-  <button class="bg-neutral-700 hover:bg-neutral-800 text-white font-bold py-2 px-4 rounded">
+  <button class="bg-neutral-300 hover:bg-neutral-400 font-bold py-2 rounded flex items-center justify-center px-4">
     {{props.title}}
   </button>
 </template>
---
+++ src/components/utils/IconButton.vue
@@ -0,0 +1,5 @@
+<template>
+  <button class="py-2 rounded flex items-center justify-center hover:bg-neutral-200">
+    <slot></slot>
+  </button>
+</template>
